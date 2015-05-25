9ce3560151903a096c9cdb2710d51393d8f31454
Alston <and_croning@hotmail.com>
2015-05-24 21:38:13 -0400

    Feature Completed

--- 0304_Using_Select.php
+++ 0304_Using_Select.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<title>Welcome</title>
+<title>304 Using a SELECT tag</title>
 <meta name="author" content="Alston" />
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <link rel="stylesheet" href="styles/basic_2.css" type="text/css" />
@@ -12,20 +12,15 @@
 
 <body>
 
-<h3>What is your name?</h3>
+<h3>College Report</h3>
 
-<form method="post" action="0303_HTML_Forms.php">
-
-	<p>
-		First Name:<br />
-		<input type="text" name="firstname" size="30" />
-	</p>
-
-	<p>
-		<input type="submit" value="Submit Information" />
-	</p>
-
-</form>
+<?php
+	$firstname = $_POST['firstname'];
+	$position = $_POST['position'];
+	
+	print "<p>You are <span class='textblue'> $firstname</span> and ";
+	print "your position at college is: <span class='textblue'> $position </span></p>";
+?>
 
 </body>
 </html>
