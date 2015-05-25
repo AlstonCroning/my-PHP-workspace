2f1e467562e414921172d02a8509f08cc766589c
Alston <and_croning@hotmail.com>
2015-05-24 21:43:55 -0400

    Files before the Feature to be added

--- 0304_Using_Select.php
+++ 0304_Using_Select.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<title>Welcome</title>
+<title>Using a SELECT tag</title>
 <meta name="author" content="Alston" />
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <link rel="stylesheet" href="styles/basic_2.css" type="text/css" />
@@ -12,20 +12,9 @@
 
 <body>
 
-<h3>What is your name?</h3>
+<h3>College Report</h3>
 
-<form method="post" action="0303_HTML_Forms.php">
 
-	<p>
-		First Name:<br />
-		<input type="text" name="firstname" size="30" />
-	</p>
-
-	<p>
-		<input type="submit" value="Submit Information" />
-	</p>
-
-</form>
 
 </body>
 </html>
