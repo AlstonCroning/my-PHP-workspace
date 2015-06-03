417474764415dc28ca05cdd9eae2c15ae0b83657
Alston <and_croning@hotmail.com>
2015-06-03 03:16:57 -0400

    Project 3 - Reading data from a text file feature added

---
+++ Assignment3/view_guestbook.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>King Real Estate - GuestBook</title>
+<meta name="author" content="Alston" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="styles/styles.css" type="text/css" />
+</head>
+
+<body>
+
+<div id="logoimage">
+	<img src="images/logo.png" />
+</div>
+
+
+
+<p class="titlepara">View GuestBook</p>
+
+
+
+</body>
+</html>
