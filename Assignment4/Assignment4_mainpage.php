d49d122059e0d3431a4cda8e90dbf39e2bcc2d44
Alston <and_croning@hotmail.com>
2015-06-08 16:03:55 -0400

    Prepared and gathered files required for Assignment4

---
+++ Assignment4/Assignment4_mainpage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>King Real Estate - Main page</title>
+	<meta name="author" content="Alston" />
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" href="styles/styles.css" type="text/css" />
+</head>
+
+<body>
+
+<div id="logoimage">
+	<a href="http://localhost/Workspace/Assignment4/Assignment4_mainpage.php"><img src="images/logo.png" /></a>
+</div>
+
+
+<p><a href="http://localhost/Workspace/Assignment4/Assignment4_guest_calc.php">GuestBook/Mortgage Calculator</a></p>
+
+</body>
+</html>
