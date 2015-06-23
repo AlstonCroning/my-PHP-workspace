0769b072ca46eda8833282f8ffb2fbd49d672594
Alston <and_croning@hotmail.com>
2015-06-22 21:15:24 -0400

    Gathering files for MouseOverEffect Demonstration with CSS

---
+++ Prep/MouseOverEffect/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Mouse Hover Effects</title>
+	<meta name="author" content="Alston" />
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" href="styles/styles.css" type="text/css" />
+</head>
+
+<body>
+
+
+
+
+
+
+
+</body>
+</html>
