7f4d1f8316e1fddbc34dcca1259e884491b2f84e
Alston <and_croning@hotmail.com>
2015-07-01 00:45:23 -0400

    Success in Receiving Data from a Database

--- Prep/MySQL/Connect-PHP-with-MySQL/index.php
+++ Prep/MySQL/Connect-PHP-with-MySQL/index.php
@@ -12,8 +12,38 @@
 </head>
 
 <body>
+<?php
+
+$db = new mysqli('localhost','root','');
+
+if(!$db) 
+{
+	print "<h1>Unable to connect to MYSQL</h1>";
+	exit();
+}
+
+$dbname = 'sampledatabase';
 
 
+$btest = mysqli_select_db($db ,$dbname);
 
+if(!$btest)
+{
+	print "<h1>Unable to Select the Database</h1>";
+}
+
+$Sql_Statement = "SELECT * ";
+$Sql_Statement .= "FROM Employee";
+
+$result = mysqli_query($db, $Sql_Statement);
+
+for($ii = 0; $ii < 2; $ii++)
+{
+	$row = mysqli_fetch_array($result);
+	print $row['firstname']."<br />";
+	print $row['lastname']."<br />";
+}
+
+?>
 </body>
 </html>
