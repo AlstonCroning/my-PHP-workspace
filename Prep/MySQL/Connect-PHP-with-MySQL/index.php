776066b1e7e15e79bca3ee93cc7f366441c7f43a
Alston <and_croning@hotmail.com>
2015-07-02 15:24:48 -0400

    comments added

--- Prep/MySQL/Connect-PHP-with-MySQL/index.php
+++ Prep/MySQL/Connect-PHP-with-MySQL/index.php
@@ -14,32 +14,41 @@
 <body>
 <?php
 
+//Using my sql improved extention
 $db = new mysqli('localhost','root','');
 
+//if unable to connect to MySQL
 if(!$db) 
 {
 	print "<h1>Unable to connect to MYSQL</h1>";
 	exit();
 }
 
+//selecting the database
 $dbname = 'sampledatabase';
-
-
 $btest = mysqli_select_db($db ,$dbname);
 
+//if unable to select the database
 if(!$btest)
 {
 	print "<h1>Unable to Select the Database</h1>";
 }
 
+//SQL statements
 $Sql_Statement = "SELECT * ";
 $Sql_Statement .= "FROM Employee";
 
+//perform a query on the database and get Results
 $result = mysqli_query($db, $Sql_Statement);
 
+//needs to determines how many times needs to loop 
+//depends on how many rows in the table
 for($ii = 0; $ii < 2; $ii++)
 {
+	//fetch a result row as an array
 	$row = mysqli_fetch_array($result);
+	
+	//Display Each Result (each time loops)
 	print $row['firstname']."<br />";
 	print $row['lastname']."<br />";
 }
